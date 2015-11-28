ba2132479b0c5691a4db41043d21f481a8dd6076
tro3373 <tro3373@gmail.com>
2015-11-28 15:55:44 +0900

    postgres dump ツールを更新！

--- tools/postgres/output_tsv.sh
+++ tools/postgres/output_tsv.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 database=hogehoge
 user=hogehoge
-outfile=hogehoge.tsv
-sql="select * form hogehoge;"
-
-psql -U $user $database -A -F"	" -c "$sql" > $outfile
+outfile=output.tsv
+args="$*"
+sql=./exe.sql
+if [ ! "$args" = "" ]; then
+    # ファイル指定がある場合はそれを実行
+    sql="$args"
+elif [ ! -e $sql ]; then
+    # execute.sql もない
+    echo "No execute.sql file exist. path=$sql"
+    exit 1
+fi
+pgpass=~/.pgpass
+if [ ! -f $pgpass ]; then
+    # .pgpass ファイル無し
+    echo "No .pgpass file exist."
+    echo "Create youre .pgpass file to home directory. like this.."
+    echo '  echo "localhost:5432:database:username:password" > ~/.pgpass'
+    echo '  echo "chmod 0600 ~/.pgpass"'
+    exit 1
+fi
+echo "========================================" >> $outfile
+echo "execute: $sql" >> $outfile
+echo "========================================" >> $outfile
+if [ -f "$sql" ]; then
+    # ファイルの場合
+    psql -U $user $databaes -A -F"	" -f "$sql" >> $outfile
+else
+    # sql の場合
+    psql -U $user $databaes -A -F"	" -c "$sql" >> $outfile
+fi
 
