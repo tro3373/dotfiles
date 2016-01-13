8bd63e2a0e699a7a631934635d561bce11602985
tro3373 <tro3373@gmail.com>
2016-01-14 02:52:20 +0900

    Add setup for golang

---
+++ tools/golang/setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+script_dir=$(cd $(dirname $0); pwd)
+dry_run=0
+
+# インストール用関数 ロード
+source ~/dotfiles/setup/setup-funcs.sh
+
+if [ "$OS" = "mac" ]; then
+    brew update
+    brew install mercurial
+elif [ "$OS" = "ubuntu" ]; then
+    sudo apt-get install curl git mercurial make binutils bison gcc build-essential
+elif [ "$OS" = "redhat" ]; then
+    sudo yum install curl git make bison gcc glibc-devel
+elif [ "$OSTYPE" = "cygwin" ]; then
+    echo "Not Support!!" 1>&2
+    exit 1
+fi
+
+# Install gvm
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+
+# echo '[[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"' >> .zshrc
+# gvm install go1.4
+# gvm use go1.4
+# gvm install go1.5
+
+# go version
+# gvm use go1.4.2 --default
+# gvm list
+# gvm listall
+
+## go setting(@see https://github.com/astaxie/build-web-application-with-golang/blob/master/ja/01.1.md)
+# export GOROOT=$HOME/go
+# export GOPATH=$HOME/gopath
+# export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+# [ dufferzafar/cheat ]( http://mattn.kaoriya.net/software/lang/go/20150518101220.htm )
+# - https://github.com/dufferzafar/cheat
+# - https://github.com/jahendrie/cheat
+# go get github.com/dufferzafar/cheat
