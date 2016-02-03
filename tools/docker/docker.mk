4be51770d9a359f2cf3959b051c82b7f92f0e967
tro3373 <tro3373@gmail.com>
2016-02-04 08:59:28 +0900

    Add notify, del duplicate tools, add docker make sample.

---
+++ tools/docker/docker.mk
@@ -0,0 +1,47 @@
+# http://qiita.com/takara@github/items/a0a295d265ab5ff43ddc
+# http://qiita.com/takara@github/items/7beee975e6df7ce53755
+all_container=`docker ps -a -q`
+active_container=`docker ps -q`
+images=`docker images | awk '/^<none>/ { print $$3 }'`
+
+all:
+
+build:
+    docker build -t $(NAME):$(VERSION) .
+
+restart: stop start
+
+start:
+    docker run -d \
+        --name $(NAME) \
+        -h $(NAME) \
+        $(DOCKER_RUN_OPTIONS) $(NAME):$(VERSION)
+
+stop:
+    docker rm -f $(NAME)
+
+logs:
+    docker logs $(NAME)
+
+attach:
+    docker exec -it $(NAME) /bin/bash --login
+
+tag:
+    docker tag $(NAME):$(VERSION) $(NAME):latest
+
+clean: clean_container clean_images
+
+clean_images:
+    @if [ "$(images)" != "" ] ; then \
+        docker rmi $(images); \
+    fi
+
+clean_container:
+    @for a in $(all_container) ; do \
+        for b in $(active_container) ; do \
+            if [ "$${a}" = "$${b}" ] ; then \
+                continue 2; \
+            fi; \
+        done; \
+        docker rm $${a}; \
+    done
