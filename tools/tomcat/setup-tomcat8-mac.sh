26505630c0288143310d2d63faa757fc1028017b
tro3373 <tro3373@gmail.com>
2016-02-06 22:08:27 +0900

    update version and url for tomcat8

--- tools/tomcat/setup-tomcat8-mac.sh
+++ tools/tomcat/setup-tomcat8-mac.sh
@@ -5,7 +5,8 @@ set -eu
 DIR=~/Downloads/Tomcat
 mkdir -p $DIR
 cd $DIR
-wget http://ftp.jaist.ac.jp/pub/apache/tomcat/tomcat-8/v8.0.28/bin/apache-tomcat-8.0.28.tar.gz
+#wget http://ftp.jaist.ac.jp/pub/apache/tomcat/tomcat-8/v8.0.28/bin/apache-tomcat-8.0.28.tar.gz
+wget http://ftp.kddilabs.jp/infosystems/apache/tomcat/tomcat-8/v8.0.30/bin/apache-tomcat-8.0.30.tar.gz
 tar xvfpz apache-tomcat-8*
 cd apache-tomcat-8*
 chmod +x bin/*.sh
