55bf16eaac9df7435a76495895c70679f7840adb
tro3373 <tro3373@gmail.com>
2016-03-09 01:09:34 +0900

    update letsencript crontab command

--- tools/letsencript/setup.sh
+++ tools/letsencript/setup.sh
@@ -16,6 +16,11 @@ main() {
     echo "==>Crontab"
     echo "  sudo crontab -e"
     echo "  # Add below.."
+    echo "  # Sample1"
     echo "  30 2 * * 1 cd $src_dir && ./letsencrypt-auto certonly --standalone -d www.example.com -d example.com --debug"
+    echo "  # Sample2"
+    echo "  00 05 01 * * $src_dir/letsencrypt-auto renew --force-renew && /bin/systemctl reload httpd"
+    echo "  # Sample3"
+    echo "  00 05 01 * * service apache2 stop && $src_dir/letsencrypt-auto renew --force-renew && service apache2 start"
 }
 main
