5084609e9c832f0e04cf0f3a538f14e3543cdb50
tro3373 <tro3373@gmail.com>
2016-03-14 01:26:47 +0900

    update ag for msys2, but not successful...

--- apps/ag/config.sh
+++ apps/ag/config.sh
@@ -8,24 +8,38 @@ install() {
     elif [ "$OSTYPE" = "cygwin" ]; then
         # 必要パッケージインストール
         dvexec "$instcmd autoconf automake gcc-g++ gettext gettext-devel liblzma-devel make mingw-gcc-g++ mingw-zlib-devel pkg-config xz zlib-devel"
-        # ソースコード取得
-        workdir="$script_dir/tmp"
-        if [ ! -e $workdir ]; then
-            dvexec "mkdir -p \"$workdir\""
-        fi
-        dvexec "cd \"$workdir\""
-        if [ ! -e "$workdir/the_silver_searcher" ]; then
-            dvexec git clone https://github.com/ggreer/the_silver_searcher.git
-        fi
-        dvexec "cd the_silver_searcher/"
-        dvexec "aclocal && autoconf && autoheader && automake --add-missing"
-        dvexec "./configure"
-        dvexec "make"
-        dvexec "make install"
-    #else
+        clone_src_cd
+        dvexec aclocal && autoconf && autoheader && automake --add-missing
+        dvexec ./configure
+        dvexec make
+        dvexec make install
+    elif [ "$OS" = "mingw" ]; then
+        # 必要パッケージインストール
+        # dvexec "$instcmd base-devel mingw-w64-{i686,x86_64}-gcc mingw-w64-{i686,x86_64}-pcre mingw-w64-{i686,x86_64}-xz"
+        # clone_src_cd
+        # dvexec ./build.sh PCRE_CFLAGS=-DPCRE_STATIC LDFLAGS=-static
+        # dvexec strip ag.exe
+        :
+        # だめならこれを試すべし
+        # http://proglab.blog.fc2.com/blog-entry-9.html
+    else
+        :
         #cmd="sudo rpm -ivh http://swiftsignal.com/packages/centos/6/x86_64/the-silver-searcher-0.13.1-1.el6.x86_64.rpm"
         #dvexec "$cmd"
     fi
+}
+
+clone_src_cd() {
+    # ソースコード取得
+    workdir="$script_dir/tmp"
+    if [ ! -e $workdir ]; then
+        dvexec "mkdir -p \"$workdir\""
+    fi
+    dvexec "cd \"$workdir\""
+    if [ ! -e "$workdir/the_silver_searcher" ]; then
+        dvexec git clone https://github.com/ggreer/the_silver_searcher.git
+    fi
+    dvexec cd the_silver_searcher/
 }
 
 setconfig() {
