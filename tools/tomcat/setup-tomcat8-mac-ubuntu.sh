60a3bc3330542272977658c3d793da57b150fa7e
tro3373 <tro3373@gmail.com>
2016-04-11 21:21:33 +0900

    update tomcat8 setup tool

--- tools/tomcat/setup-tomcat8-mac-ubuntu.sh
+++ tools/tomcat/setup-tomcat8-mac-ubuntu.sh
@@ -6,7 +6,8 @@ DIR=~/Downloads/Tomcat
 mkdir -p $DIR
 cd $DIR
 #wget http://ftp.jaist.ac.jp/pub/apache/tomcat/tomcat-8/v8.0.28/bin/apache-tomcat-8.0.28.tar.gz
-wget http://ftp.kddilabs.jp/infosystems/apache/tomcat/tomcat-8/v8.0.30/bin/apache-tomcat-8.0.30.tar.gz
+#wget http://ftp.kddilabs.jp/infosystems/apache/tomcat/tomcat-8/v8.0.30/bin/apache-tomcat-8.0.30.tar.gz
+wget http://ftp.riken.jp/net/apache/tomcat/tomcat-8/v8.0.33/bin/apache-tomcat-8.0.33.tar.gz
 tar xvfpz apache-tomcat-8*
 cd apache-tomcat-8*
 chmod +x bin/*.sh
