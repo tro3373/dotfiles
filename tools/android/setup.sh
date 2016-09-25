cb245b50d79c0f58d3a9082e30d2961febd381ca
tro3373 <tro3373@gmail.com>
2016-09-25 22:11:57 +0900

    Add install lib to android studio setup.

--- tools/android/setup.sh
+++ tools/android/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Install java
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
@@ -8,3 +9,6 @@ sudo apt-get install oracle-java8-installer
 
 sudo apt-get install libc6-i386 libncurses5:i386 libstdc++6:i386
 
+# Install android studio build libs
+sudo apt-get install lib32stdc++6
+sudo apt-get install lib32z1
