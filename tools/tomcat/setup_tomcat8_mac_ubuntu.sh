4245fbc791d4f968dadfdf1b65b7c839af04075b
tro3373 <tro3373@gmail.com>
2016-10-13 00:14:37 +0900

    udpate tomcat install version

--- tools/tomcat/setup_tomcat8_mac_ubuntu.sh
+++ tools/tomcat/setup_tomcat8_mac_ubuntu.sh
@@ -7,7 +7,9 @@ mkdir -p $DIR
 cd $DIR
 #wget http://ftp.jaist.ac.jp/pub/apache/tomcat/tomcat-8/v8.0.28/bin/apache-tomcat-8.0.28.tar.gz
 #wget http://ftp.kddilabs.jp/infosystems/apache/tomcat/tomcat-8/v8.0.30/bin/apache-tomcat-8.0.30.tar.gz
-wget http://ftp.riken.jp/net/apache/tomcat/tomcat-8/v8.0.33/bin/apache-tomcat-8.0.33.tar.gz
+#wget http://ftp.riken.jp/net/apache/tomcat/tomcat-8/v8.0.33/bin/apache-tomcat-8.0.33.tar.gz
+#wget http://ftp.riken.jp/net/apache/tomcat/tomcat-8/v8.5.6/bin/apache-tomcat-8.5.6.tar.gz
+wget http://ftp.riken.jp/net/apache/tomcat/tomcat-8/v8.0.38/bin/apache-tomcat-8.0.38.tar.gz
 tar xvfpz apache-tomcat-8*
 cd apache-tomcat-8*
 chmod +x bin/*.sh
