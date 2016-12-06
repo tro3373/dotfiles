895883b3af0a90574bb4d55b79b2c47ed5503326
tro3373 <tro3373@gmail.com>
2016-12-07 00:15:00 +0900

    Update vim path for msys/windows

--- tools/win/bin/gvim.bat
+++ tools/win/bin/gvim.bat
@@ -3,5 +3,10 @@ set args="--remote-tab-silent %*"
 if "%1" == "" (
     set args="."
 )
+set path=C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;
+rem set path=%HOMEPATH%\AppData\Local\Msys64\usr\bin;%path%
+set path=%HOMEPATH%\AppData\Local\Msys64\usr\bin\ag.exe;%path%
+set path=C:\Program Files\Git\cmd;%path%
+set path=%HOMEPATH%\bin;%path%
 start "" "C:%HOMEPATH%\tools\vim80-kaoriya-win64\gvim.exe %args%"
 
