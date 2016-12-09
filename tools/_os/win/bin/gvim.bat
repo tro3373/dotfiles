b1a24d2dad40975587e6a7182975f4c30a29b563
tro3373 <tro3373@gmail.com>
2016-12-10 04:33:50 +0900

    BugFix start gvim with args(directory is buggy yet)

--- tools/_os/win/bin/gvim.bat
+++ tools/_os/win/bin/gvim.bat
@@ -1,7 +1,7 @@
 @echo off
-set args="%*"
+set args=%*
 if "%1" == "" (
-    set args="."
+    set args=%cd%
 )
 set gvim_dir=vim80-kaoriya-win64
 set path=C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;
@@ -12,5 +12,6 @@ rem set path=%HOMEPATH%\AppData\Local\Msys64\usr\bin\git.exe;%path%
 set path=C:\Program Files\Git\cmd;%path%
 set path=%HOMEPATH%\AppData\Local\Msys64\Mingw64\bin;%path%
 set path=%HOMEPATH%\bin;%path%
+rem echo %path%
 start "" C:%HOMEPATH%\tools\%gvim_dir%\gvim.exe --remote-tab-silent %args%
 
