e01e33ea41791caec9fe6946bed7372949b8282d
tro3373 <tro3373@gmail.com>
2016-12-09 16:50:56 +0900

    Bugfix for gvim for winpath

--- tools/_os/win/bin/gvim.bat
+++ tools/_os/win/bin/gvim.bat
@@ -1,12 +1,16 @@
 @echo off
-set args="--remote-tab-silent %*"
+set args="%*"
 if "%1" == "" (
     set args="."
 )
+set gvim_dir=vim80-kaoriya-win64
 set path=C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;
+rem GitGutter Not work
 rem set path=%HOMEPATH%\AppData\Local\Msys64\usr\bin;%path%
-set path=%HOMEPATH%\AppData\Local\Msys64\usr\bin\ag.exe;%path%
+rem rem Fullpath Not work
+rem set path=%HOMEPATH%\AppData\Local\Msys64\usr\bin\git.exe;%path%
 set path=C:\Program Files\Git\cmd;%path%
+set path=%HOMEPATH%\AppData\Local\Msys64\Mingw64\bin;%path%
 set path=%HOMEPATH%\bin;%path%
-start "" "C:%HOMEPATH%\tools\vim80-kaoriya-win64\gvim.exe %args%"
+start "" C:%HOMEPATH%\tools\%gvim_dir%\gvim.exe --remote-tab-silent %args%
 
