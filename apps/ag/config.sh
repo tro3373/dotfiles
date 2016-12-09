45f3ff1d0e84b04c99422dddfd95ef87ee43e143
tro3373 <tro3373@gmail.com>
2016-12-09 18:17:23 +0900

    Ag build not work yet

--- apps/ag/config.sh
+++ apps/ag/config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 install() {
+    url=https://github.com/ggreer/the_silver_searcher.git
     if [ "$DETECT_OS" = "mac" ]; then
         dvexec "$instcmd ag"
     elif [ "$DETECT_OS" = "ubuntu" ]; then
@@ -8,42 +9,50 @@ install() {
     elif [ "$DETECT_OS" = "cygwin" ]; then
         # 必要パッケージインストール
         dvexec "$instcmd autoconf automake gcc-g++ gettext gettext-devel liblzma-devel make mingw-gcc-g++ mingw-zlib-devel pkg-config xz zlib-devel"
-        clone_src_cd
+        workdir="$script_dir/tmp"
+        if [ ! -e $workdir ]; then
+            dvexec "mkdir -p \"$workdir\""
+        fi
+        dvexec "cd \"$workdir\""
+        dvexec git clone $url
+        dvexec cd the_silver_searcher/
         dvexec aclocal && autoconf && autoheader && automake --add-missing
         dvexec ./configure
         dvexec make
         dvexec make install
     elif [ "$DETECT_OS" = "msys" ]; then
-        dvexec "$instcmd mingw-w64-x86_64-ag"
+        local type=package # build/zip/package
+        if [ "$type" = "package" ]; then
+            dvexec "$instcmd mingw-w64-x86_64-ag"
+            return 0
+        fi
         workdir="$script_dir/tmp"
         if [ ! -e $workdir ]; then
             dvexec "mkdir -p \"$workdir\""
         fi
-        if [ ! -e $workdir/ag.zip ]; then
-            dvexec "cd \"$workdir\""
-            dvexec wget https://kjkpub.s3.amazonaws.com/software/the_silver_searcher/rel/0.29.1-1641/ag.zip
+        dvexec "cd \"$workdir\""
+        if [ "$type" = "zip" ]; then
+            if [ ! -e $workdir/ag.zip ]; then
+                dvexec wget https://kjkpub.s3.amazonaws.com/software/the_silver_searcher/rel/0.29.1-1641/ag.zip
+            fi
             dvexec unzip ag.zip
-            #dvexec mv ag/ag.exe $HOME/bin
+            dvexec mv ag/ag.exe $HOME/bin
             dvexec "cd -"
+            return 0
         fi
+        # for build
+        dvexec $instcmd base-devel mingw-w64-x86_64-gcc mingw-w64-x86_64-pcre mingw-w64-x86_64-xz
+        if [ ! -e the_silver_searcher/.git ]; then
+            dvexec git clone $url
+        fi
+        dvexec cd the_silver_searcher/
+        dvexec ./build.sh PCRE_CFLAGS=-DPCRE_STATIC LDFLAGS=-static
+        dvexec strip ag.exe
     else
         :
         #cmd="sudo rpm -ivh http://swiftsignal.com/packages/centos/6/x86_64/the-silver-searcher-0.13.1-1.el6.x86_64.rpm"
         #dvexec "$cmd"
     fi
-}
-
-clone_src_cd() {
-    # ソースコード取得
-    workdir="$script_dir/tmp"
-    if [ ! -e $workdir ]; then
-        dvexec "mkdir -p \"$workdir\""
-    fi
-    dvexec "cd \"$workdir\""
-    if [ ! -e "$workdir/the_silver_searcher" ]; then
-        dvexec git clone https://github.com/ggreer/the_silver_searcher.git
-    fi
-    dvexec cd the_silver_searcher/
 }
 
 setconfig() {
@@ -52,3 +61,4 @@ setconfig() {
         make_link_bkupable "/mingw64/bin/ag.exe" "${HOME}/bin/ag.exe"
     fi
 }
+
