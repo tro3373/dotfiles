139eb1afbbe50198a661004865f270d8317ba9da
tro3373 <tro3373@gmail.com>
2017-02-12 14:11:47 +0900

    Add udev rules for nexus 5x/6p

--- tools/android/setup_51-android_rules.sh
+++ tools/android/setup_51-android_rules.sh
@@ -2,9 +2,9 @@
 
 rules=/etc/udev/rules.d/51-android.rules
 if [[ -e $rules ]]; then
-    echo "Already exist rules. $rules"
-    exit 1
+    echo "Already exist rules. $rules continue?"
+    read force_setup
 fi
-sudo cp ./etc_udev_rules.d_51-android.rules $rules
+sudo cp -fv ./etc_udev_rules.d_51-android.rules $rules
 sudo service udev restart
 echo done
