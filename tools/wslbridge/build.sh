1975ac4a671b0a35274f2ed27d8e6759fb57a930
tro3373 <tro3373@gmail.com>
2017-03-02 17:12:55 +0900

    update

--- tools/wslbridge/build.sh
+++ tools/wslbridge/build.sh
@@ -12,19 +12,39 @@ clone_if_needed() {
         git clone $url
     fi
 }
+
+update_build_package() {
+    pacman -S patch
+    pacman -S mingw-w64-x86_64-toolchain
+    pacman -S make gcc git
+}
+
 front() {
     cd ./wslbridge/frontend
+    # update_build_package
     make
 
-    mintty_winpath=$(cygpath -aw $(which mintty))
+    # mintty_winpath=$(cygpath -aw $(which mintty))
+    mintty_winpath=$(cygpath -aw $(dirname $(which mintty)))
     winbin=$(cygpath -aw $WINHOME/bin)
     cp ../out/wslbridge.exe $winbin
-    echo "$mintty_winpath -t \"Bash on Ubuntu on Windows\" -e ./wslbridge.exe" >> $winbin/wsl.bat
+    cat <<EOL>$winbin/wsl.bat
+@echo off
+cd $mintty_winpath
+mintty.exe -t "Bash on Ubuntu on Windows" -e $winbin\wslbridge.exe
+EOL
+    # echo "@echo off" > $winbin/wsl.bat
+    # # echo "$mintty_winpath -t \"Bash on Ubuntu on Windows\" -e $winbin\\wslbridge.exe" >> $winbin/wsl.bat
+    # echo "cd $mintty_winpath"
+    # echo "mintty -t \"Bash on Ubuntu on Windows\" -e $winbin\\wslbridge.exe" >> $winbin/wsl.bat
+    unix2dos $winbin/wsl.bat
 }
 
 back() {
     cd ./wslbridge/backend
     make
+    winbin=/mnt/c/Users/`whoami`/bin
+    cp ../out/wslbridge-backend $winbin
 }
 
 main() {
