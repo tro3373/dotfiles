827f11e72ccc7b572f177ec8425eb6f5a634e7b7
tro3373 <tro3373@gmail.com>
2017-03-05 00:22:47 +0900

    Bugfix for setup goenv

--- tools/golang/setup_goenv_via_anyenv.sh
+++ tools/golang/setup_goenv_via_anyenv.sh
@@ -2,7 +2,7 @@
 
 main() {
     [ ! type anyenv >/dev/null 2>&1 ] && echo "No anyenv exist." && exit 2
-    if [ ! type goenv >/dev/null 2>&1 ]; then
+    if ! type goenv >/dev/null 2>&1; then
         anyenv install goenv
         echo "===> Reload shell"
     else
