8ade564210ecc2e8119917f9ea24bdb80f29c37a
tro3373 <tro3373@gmail.com>
2017-03-08 02:40:20 +0900

    update docker install tool

--- tools/docker/setup/setup_docker_for_ubuntu.sh
+++ tools/docker/setup/setup_docker_for_ubuntu.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+old_install() {
+    # Install Docker
+    wget -qO- https://get.docker.com/ | sudo sh
+    # version を確認しておく。
+    docker --version
+}
+
+old_uninstall() {
+    sudo apt-get remove docker docker-engine
+}
+
 install_via_curl() {
     url="$1"
     command_name="$2"
@@ -9,18 +20,50 @@ install_via_curl() {
     $command_name --version
 }
 
-# Install Docker
-wget -qO- https://get.docker.com/ | sudo sh
-# version を確認しておく。
-docker --version
-# ユーザーを docker グループに追加
-sudo usermod -aG docker $USER
+install_compose() {
+    # Install Docker Compose
+    compose_version="1.11.2"
+    install_via_curl "https://github.com/docker/compose/releases/download/$compose_version/docker-compose-`uname -s`-`uname -m`" docker-compose
+}
 
-# Install Docker Machine
-machine_version="v0.8.0"
-install_via_curl "curl -L https://github.com/docker/machine/releases/download/$machine_version/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine" docker-machine
+install_machine() {
+    # Install Docker Machine
+    machine_version="v0.10.0"
+    install_via_curl "curl -L https://github.com/docker/machine/releases/download/$machine_version/docker-machine-`uname -s`-`uname -m`" docker-machine
+}
 
-# Install Docker Compose
-compose_version="1.8.0"
-install_via_curl "https://github.com/docker/compose/releases/download/$compose_version/docker-compose-`uname -s`-`uname -m`" docker-compose
+uninstall() {
+    sudo apt-get purge docker-ce
+}
 
+install_docker() {
+    # @see https://docs.docker.com/engine/installation/linux/ubuntu/#install-docker
+    # Install packages to allow apt to use a repository over HTTPS:
+    sudo apt-get install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        software-properties-common
+    # Add Docker’s official GPG key:
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
+    sudo apt-get -y update
+    sudo apt-get -y install docker-ce
+    sudo docker run hello-world
+}
+
+mod_user() {
+    # ユーザーを docker グループに追加
+    sudo usermod -aG docker $USER
+}
+
+main() {
+    install_docker
+    install_machine
+    install_compose
+    mod_user
+}
+main
