2f9388e9fade63d2a23a051c6753b5c557e526c2
tro3373 <tro3373@gmail.com>
2017-03-11 17:04:01 +0900

    Update new template

--- bin/lib/template.sh
+++ bin/lib/template.sh
@@ -1,14 +1,52 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 current_dir=$(pwd)
 script_dir=$(cd $(dirname $0); pwd)
+command_name=$(basename $0)
+
+usage() {
+    cat <<EOF
+Description for this command.
+  Usage:
+      $command_name [option]
+    Options
+      -h|--help : Show this usage
+EOF
+}
 
 has() {
     which ${1} >/dev/null 2>&1
     return $?
 }
 
-main() {
-    :
+add_args() {
+    args=("${args[@]}" "$@")
 }
-main $*
+
+set_args() {
+    for arg in "$@"; do
+        case "$arg" in
+            -h|--help)
+                usage
+                exit 0
+                ;;
+            *)
+                add_args "$arg"
+                ;;
+        esac
+    done
+}
+
+initialize() {
+    args=()
+    set_args "$@"
+}
+
+main() {
+    initialize "$@"
+    for arg in "${args[@]}"; do
+        echo "Arguments: $arg"
+    done
+}
+main "$@"
+
