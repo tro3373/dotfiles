feee1dff3284eb55da45aa6f5e0ca0cf533d31c4
tro3373 <tro3373@gmail.com>
2018-03-23 12:39:43 +0900

    feat: new prompt color

--- apps/zsh/.zsh/20.base.zsh
+++ apps/zsh/.zsh/20.base.zsh
@@ -181,8 +181,8 @@ case ${UID} in
     #PROMPT=$'%{\e[38;5;030m%}%m%(!.#.$)%{\e[m%} '
     # cyan
     #PROMPT=$'%{\e[38;5;030m%}%/$%{\e[m%} '
-    PROMPT=$'%{\e[38;5;%(?.033.013)m%}%/>%{\e[m%} '
-    PROMPT2=$'%{\e[38;5;%(?.033.013)m%}%_> %{\e[m%} '
+    PROMPT=$'%{\e[38;5;%(?.012.013)m%}%/>%{\e[m%} '
+    PROMPT2=$'%{\e[38;5;%(?.012.013)m%}%_> %{\e[m%} '
     SPROMPT="%{${fg[red]}%}%r is correct? [n,y,a,e]:%{${reset_color}%} "
     [ -n "${REMOTEHOST}${SSH_CONNECTION}" ] &&
         #PROMPT="%{${fg[magenta]}%}$(echo ${HOST%%.*} | tr '[a-z]' '[A-Z]') ${PROMPT}"
