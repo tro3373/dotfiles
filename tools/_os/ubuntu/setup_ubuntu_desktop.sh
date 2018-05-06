630a960a8e75af58f9f1d0a3e6298e03237e12d9
tro3373 <tro3373@gmail.com>
2018-05-07 03:34:36 +0900

    refactore: upgrade ubuntu descktop setup

--- tools/_os/ubuntu/setup_ubuntu_desktop.sh
+++ tools/_os/ubuntu/setup_ubuntu_desktop.sh
@@ -1,96 +1,76 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -eu
-
-script_dir=$(cd $(dirname $0); pwd)
-DOTPATH=${DOTPATH:-$script_dir}
-
-# Settings for 2byte characters
-# @see https://wiki.ubuntulinux.jp/hito/WIP-ambiwidth
-# sudo bash -c 'echo "export VTE_CJK_WIDTH=1" > /etc/profile.d/vte_cjk_width.sh'
-# For Under ubuntu14.10
-# ln -s $DOTPATH/tools/ubuntu/cjk-terminal.desktop ~/.local/share/applications/cjk-terminal.desktop
-# For Upper ubuntu15.04, terminal-app settings can do that.
-# [Profile settings - compatibility - fazy width char] => set to double byte
-#   => but, tmux not fix it, buggy....
-
-# Cpas Lock => Ctrl setting
-if [ type dconf > /dev/null 2>&1 ]; then
-    dconf reset /org/gnome/settings-daemon/plugins/keyboard/active
-    dconf write /org/gnome/settings-daemon/plugins/keyboard/active true
-    dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:nocaps']"
-    #dconf reset /org/gnome/desktop/input-sources/xkb-options
-fi
-
-# Disable automount external media
-gsettings set org.gnome.desktop.media-handling automount false
-
-# xsel, meld, rapidsvn, smartmontools
-sudo apt-get install -y xsel meld rapidsvn smartmontools hardinfo
-# smartctl -s on /dev/sda
-
-if [ ! -e ~/Desktop ]; then
+setup_home_dir_names() {
+    if [ -e ~/Desktop ]; then
+        return
+    fi
     # 日本語ディレクトリ名を英語化
     env LANGUAGE=C LC_MESSAGES=C xdg-user-dirs-gtk-update
-fi
-# visudo エディタをvimに設定
-sudo apt-get install -y vim
-sudo update-alternatives --config editor
+}
 
-# Tweak Tool
-sudo apt-get install -y unity-tweak-tool gnome-tweak-tool
-# Numix Icon theme
-sudo apt-add-repository -y ppa:numix/ppa
-sudo apt-get update
-# support For ubuntu 16.04
-#sudo apt-get install -y numix-gtk-theme numix-icon-theme numix-wallpaper-saucy numix-icon-theme-circle
-sudo apt-get install -y numix-gtk-theme numix-icon-theme numix-icon-theme-circle
+setup_capslocks() {
+    if ! has dconf; then
+        log "==> No dconf command exist."
+        return
+    fi
+    # Cpas Lock => Ctrl setting
+    dconf reset /org/gnome/settings-daemon/plugins/keyboard/active
+    dconf write /org/gnome/settings-daemon/plugins/keyboard/active true
+    dconf reset /org/gnome/desktop/input-sources/xkb-options
+    dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:nocaps']"
+    #dconf reset /org/gnome/desktop/input-sources/xkb-options
+}
 
-# Guake install
-#sudo apt-get install -y guake
+setup_mount_media_setting() {
+    # Disable automount external media
+    gsettings set org.gnome.desktop.media-handling automount false
+}
 
-# y-ppa-manager for fix GPG keys errors.
-#sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager
-#sudo apt-get update
-#sudo apt-get install -y y-ppa-manager
+install_apps() {
+    # Chrome install
+    _update=0
+    if ! sudo test -e /etc/apt/sources.list.d/google-chrome.list; then
+        wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+        sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+        _update=1
+    fi
+    # Numix Icon theme
+    if ! ls /etc/apt/sources.list.d |grep numix >& /dev/null; then
+        sudo apt-add-repository -y ppa:numix/ppa
+        _update=1
+    fi
 
-# Chrome install
-if ! type google-chrome-stable > /dev/null 2>&1; then
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-    sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-    sudo apt-get update
+    if [[ $_update -eq 1 ]]; then
+        sudo apt-get update
+    fi
+
     sudo apt-get install -y google-chrome-stable
-fi
+    sudo apt-get install -y numix-gtk-theme numix-icon-theme numix-icon-theme-circle
 
-## sublime install
-# if ! type subl > /dev/null 2>&1; then
-#     sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-#     sudo apt-get update
-#     sudo apt-get install -y sublime-text-installer ibus-mozc emacs-mozc
-#     cd $DOTPATH/tools/sublime
-#     ./setup.sh
-#     cd -
-# fi
+    # Tweak Tool
+    sudo apt-get install -y unity-tweak-tool gnome-tweak-tool
 
-## atom install
-#if ! type apm > /dev/null 2>&1; then
-#    sudo add-apt-repository -y ppa:webupd8team/atom
-#    sudo apt-get update
-#    sudo apt-get install -y atom
-#    cd $DOTPATH/tools/atom
-#    ./setup.sh
-#    ./install_from_package_list.sh
-#    cd -
-#fi
+    # xsel, meld, rapidsvn, smartmontools
+    sudo apt-get install -y xsel meld rapidsvn smartmontools hardinfo
+
+    # Guake install
+    #sudo apt-get install -y guake
+
+    # y-ppa-manager for fix GPG keys errors.
+    #sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager
+    #sudo apt-get update
+    #sudo apt-get install -y y-ppa-manager
+}
 
 # geany install
-geany_install() {
+setup_geany() {
     # support For ubuntu 16.04
     # sudo add-apt-repository -y ppa:geany-dev/ppa
     # sudo apt-get update
     # sudo apt-get upgrade
     sudo apt-get install -y geany
     if [ "" = "false" ]; then
+        script_dir=$(cd $(dirname $0); pwd)
         geanyconfdir=$HOME/.config/geany
         mv "$geanyconfdir/geany.conf" "$geanyconfdir/geany.conf.org"
         mv "$geanyconfdir/keybindings.conf" "$geanyconfdir/keybindings.conf.org"
@@ -114,7 +94,22 @@ geany_install() {
         echo "selection=0x000;0x33B5E5;false;true"
     fi
 }
-if ! type geany > /dev/null 2>&1; then
-    geany_install
-fi
+
+setup_default_editor() {
+    if ! has vim; then
+        sudo apt-get install -y vim
+    fi
+    # visudo エディタをvimに設定
+    sudo update-alternatives --config editor
+}
+
+main () {
+    setup_capslocks
+    setup_home_dir_names
+    setup_default_editor
+    setup_mount_media_setting
+    # smartctl -s on /dev/sda
+    install_apps
+}
+main "$@"
 
