3e7327abe1437bd2dbd58aeeb3786a5bb54c6a80
tro3373 <tro3373@gmail.com>
2019-04-10 00:36:27 +0900

    feat: win choco setting update

--- tools/_os/win/choco/setup.bat
+++ tools/_os/win/choco/setup.bat
@@ -4,33 +4,35 @@
 if errorlevel 1 powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 cd /d %~dp0
 
-rem sakura
-rem gvim
+cinst -y git.install --params "'/Tasks:quicklaunch'"
+cinst -y 7zip
+cinst -y winmerge --params "'/Tasks:quicklaunch,fileassoc,sendto'"
+cinst -y sakuraeditor --params "'/Tasks:quicklaunch,fileassoc,startup,sendto'"
+cinst -y googlechrome --params "'/Tasks:quicklaunch,fileassoc'"
+cinst -y firefox
+cinst -y googlejapaneseinput
+cinst -y tortoisesvn
+cinst -y mpc-hc
+cinst -y winscp
+cinst -y virtualbox --params "'/Tasks:quicklaunch'"
+cinst -y vagrant
+
+rem cinst -y msys2 --params "'/Tasks:quicklaunch'"
+
+rem cinst -y jdk8
+rem cinst -y android-sdk
+rem cinst -y vmwareplayer
+rem cinst -y teraterm
+rem cinst -y SublimeText3
+rem cinst -y atom
+rem cinst -y gvim
 rem cinst -y sourcetree
 rem cinst -y svn
-rem cinst -y tortoisesvn
 rem cinst -y thunderbird
-rem cinst -y msys2
-rem cinst -y jdk8
 rem cinst -y putty
 rem cinst -y ruby
 rem cinst -y python
-rem cinst -y android-sdk
-rem cinst -y vmwareplayer
-rem cinst -y virtualbox
-rem cinst -y vagrant
-rem cinst -y mpc-hc
-rem cinst -y teraterm
-rem cinst -y winscp
 
-cinst -y git.install
-cinst -y 7zip
-cinst -y winmerge
-cinst -y GoogleChrome
-cinst -y Firefox
-cinst -y SublimeText3
-cinst -y atom
-cinst -y googlejapaneseinput
 
 echo "Done"
 pause
