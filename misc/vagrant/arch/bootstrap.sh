fb74894966131c5d8d10f80d2f8290fca1c4c888
tro3373 <tro3373@gmail.com>
2020-10-23 22:09:24 +0900

    refactor: improve setup

---
+++ misc/vagrant/arch/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+readonly bootstrapped_path=/etc/bootstrapped
+
+bootstrapped() { test -f $bootstrapped_path; }
+finalize() { date | sudo tee $bootstrapped_path >/dev/null; }
+
+setup_dot() {
+  echo "==> setupping .dot .."
+  if [[ ! -e ~/.dot ]]; then
+    curl -fSsL git.io/tr3s | bash
+  fi
+  cd ~/.dot/bin
+  ./setup -e vag_arch
+}
+
+main() {
+  bootstrapped && return
+  setup_dot
+  finalize
+}
+main "$@"
