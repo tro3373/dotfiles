7728051c5091012434202450a19fb78f64f9737a
tro3373 <tro3373@gmail.com>
2022-03-15 17:48:04 +0900

    fix?: csv data syntax maybe..(not work?)

--- bin/lib/bootstrap/msys2/sudo_zsh.bat
+++ bin/lib/bootstrap/msys2/sudo_zsh.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell start-process -verb runas "$env:USERPROFILE/scoop/apps/msys2/current/msys2_shell.cmd" -ArgumentList '-shell zsh -mingw64'
+powershell start-process -verb runas "$env:USERPROFILE/scoop/apps/msys2/current/msys2_shell.cmd" -ArgumentList '-shell,zsh,-mingw64'
 exit
