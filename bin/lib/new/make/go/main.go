1392ae222c8540102e86d04dfe853ac5d8c06d0f
tro3373 <tro3373@gmail.com>
2023-08-15 14:44:55 +0900

    feat(bin): new: Improve Makefile,go template

--- bin/lib/new/make/go/main.go
+++ bin/lib/new/make/go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"os"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -11,6 +12,12 @@ func sub() error {
 }
 
 func main() {
+	// initConfigInner
+	level, err := log.ParseLevel(os.Getenv("LOG_LEVEL"))
+	if err == nil {
+		log.SetLevel(level)
+	}
+	log.Infof("==> Start")
 	if err := sub; err != nil {
 		log.Error("==> Error occured: %w", err)
 		panic(err)
