df7b9e476f1a00af25b4e5369d71534a38a7c38a
tro3373 <tro3373@gmail.com>
2023-12-04 06:01:26 +0900

    refactor(setup): docker.mk

--- apps/docker/docker.mk
+++ apps/docker/docker.mk
@@ -7,41 +7,41 @@ images=`docker images | awk '/^<none>/ { print $$3 }'`
 all:
 
 build:
-    docker build -t $(NAME):$(VERSION) .
+	docker build -t $(NAME):$(VERSION) .
 
 restart: stop start
 
 start:
-    docker run -d \
-        --name $(NAME) \
-        -h $(NAME) \
-        $(DOCKER_RUN_OPTIONS) $(NAME):$(VERSION)
+	docker run -d \
+		--name $(NAME) \
+		-h $(NAME) \
+		$(DOCKER_RUN_OPTIONS) $(NAME):$(VERSION)
 
 stop:
-    docker rm -f $(NAME)
+	docker rm -f $(NAME)
 
 logs:
-    docker logs $(NAME)
+	docker logs $(NAME)
 
 attach:
-    docker exec -it $(NAME) /bin/bash --login
+	docker exec -it $(NAME) /bin/bash --login
 
 tag:
-    docker tag $(NAME):$(VERSION) $(NAME):latest
+	docker tag $(NAME):$(VERSION) $(NAME):latest
 
 clean: clean_container clean_images
 
 clean_images:
-    @if [ "$(images)" != "" ] ; then \
-        docker rmi $(images); \
-    fi
+	@if [ "$(images)" != "" ] ; then \
+		docker rmi $(images); \
+	fi
 
 clean_container:
-    @for a in $(all_container) ; do \
-        for b in $(active_container) ; do \
-            if [ "$${a}" = "$${b}" ] ; then \
-                continue 2; \
-            fi; \
-        done; \
-        docker rm $${a}; \
-    done
+	@for a in $(all_container) ; do \
+		for b in $(active_container) ; do \
+			if [ "$${a}" = "$${b}" ] ; then \
+				continue 2; \
+			fi; \
+		done; \
+		docker rm $${a}; \
+	done
